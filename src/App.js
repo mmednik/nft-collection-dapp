402045a19b0190743b76799dc6a19cd58628bbdf
mmednik <mmednik@gmail.com>
2022-03-21 23:20:46 -0300

    metadata

--- src/App.js
+++ src/App.js
@@ -1,24 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Contract, ethers } from "ethers";
+import { useEffect, useState } from "react";
+import contractABI from "./contractABI.json";
+
+const contractAddress = "0x0761D9dCd237Fa57A27B146ce610037C0D54Fe3A";
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const [isWalletInstalled, setIsWalletInstalled] = useState(false);
+  const [NFTContract, setNFTContract] = useState(null);
+  const [isMinting, setIsMinting] = useState(false);
+
+  useEffect(() => {
+    if (window.ethereum) {
+      setIsWalletInstalled(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    function initNFTContract() {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      setNFTContract(new Contract(contractAddress, contractABI.abi, signer));
+    }
+    initNFTContract();
+  }, [account]);
+
+  async function connectWallet() {
+    window.ethereum
+      .request({
+        method: "eth_requestAccounts",
+      })
+      .then((accounts) => {
+        setAccount(accounts[0]);
+      })
+      .catch((error) => {
+        alert("Something went wrong");
+      });
+  }
+
+  const data = [
+    {
+      url: "./assets/images/1.png",
+      param: "handleMint('https://gateway.pinata.cloud/ipfs/QmRZDHF5A1VoW4qF87TyYJwwYxwT6VLbejE8txKKY7pR9g/1')",
+    },
+    {
+      url: "./assets/images/2.png",
+      param: "handleMint('https://gateway.pinata.cloud/ipfs/QmRZDHF5A1VoW4qF87TyYJwwYxwT6VLbejE8txKKY7pR9g/2')",
+    },
+    {
+      url: "./assets/images/3.png",
+      param: "handleMint('https://gateway.pinata.cloud/ipfs/QmRZDHF5A1VoW4qF87TyYJwwYxwT6VLbejE8txKKY7pR9g/3')",
+    },
+    {
+      url: "./assets/images/4.png",
+      param: "handleMint('https://gateway.pinata.cloud/ipfs/QmRZDHF5A1VoW4qF87TyYJwwYxwT6VLbejE8txKKY7pR9g/4')",
+    },
+    {
+      url: "./assets/images/5.png",
+      param: "handleMint('https://gateway.pinata.cloud/ipfs/QmRZDHF5A1VoW4qF87TyYJwwYxwT6VLbejE8txKKY7pR9g/5')",
+    },
+  ];
+
+  async function withdrawMoney() {
+    try {
+      const response = await NFTContract.withdrawMoney();
+      console.log("Received: ", response);
+    } catch (err) {
+      alert(err);
+    }
+  }
+
+  async function handleMint(tokenURI) {
+    setIsMinting(true);
+    try {
+      const options = { value: ethers.utils.parseEther("0.01") };
+      const response = await NFTContract.mintNFT(tokenURI, options);
+      console.log("Received: ", response);
+    } catch (err) {
+      alert(err);
+    } finally {
+      setIsMinting(false);
+    }
+  }
+
+  if (account === null) {
+    return (
+      <>
+        <div className="container">
+          <br />
+          <h1> 🔮 metaschool</h1>
+          <h2>NFT Marketplace</h2>
+          <p>Buy an NFT from our marketplace.</p>
+
+          {isWalletInstalled ? (
+            <button onClick={connectWallet}>Connect Wallet</button>
+          ) : (
+            <p>Install Metamask wallet</p>
+          )}
+        </div>
+      </>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="container">
+        <br />
+        <h1> 🔮 metaschool</h1>
+
+        <h2>NFT Marketplace</h2>
+        {data.map((item, index) => (
+          <div className="imgDiv">
+            <img
+              src={item.url}
+              key={index}
+              alt="images"
+              width={250}
+              height={250}
+            />
+            <button
+              isLoading={isMinting}
+              onClick={() => {
+                eval(item.param);
+              }}
+            >
+              Mint - 0.01 eth
+            </button>
+          </div>
+        ))}
+        <button
+          onClick={() => {
+            withdrawMoney();
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Withdraw Money from Contract
+        </button>
+      </div>
+    </>
   );
 }
 
